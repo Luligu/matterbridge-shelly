25d03aeb7eb89f147ab27c878f126da7defb80aa
Luligu <132135057+Luligu@users.noreply.github.com>
2024-09-24 22:24:24 +0200

    Jest wsClient

--- src/wsClient.test.ts
+++ src/wsClient.test.ts
@@ -1,30 +1,61 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-
 import { jest } from '@jest/globals';
-import { waiter } from 'matterbridge/utils';
+import { wait, waiter } from 'matterbridge/utils';
 import { WsClient } from './wsClient';
-import WebSocket from 'ws';
+import { WebSocket, WebSocketServer } from 'ws';
 import { AnsiLogger, LogLevel } from 'matterbridge/logger';
 
-jest.useFakeTimers();
-
-describe('WS client', () => {
+describe('ShellyWsClient with server', () => {
   let wsClient: WsClient;
+  let server: WebSocketServer;
 
-  beforeAll(() => {
+  beforeAll(async () => {
     // Mock the AnsiLogger.log method
     jest.spyOn(AnsiLogger.prototype, 'log').mockImplementation((level: string, message: string, ...parameters: any[]) => {
       // console.log(`Mocked log: ${level} - ${message}`, ...parameters);
     });
-    jest.spyOn(WebSocket.prototype, 'ping').mockImplementation(() => {
-      // console.log(`Mocked ping`);
+
+    // Create a WebSocket server and await its listening state
+    await new Promise<void>((resolve) => {
+      server = new WebSocketServer({ port: 80 }, () => {
+        const port = (server.address() as { port: number }).port;
+        // console.log('Jest ws server is up and listening on port:', port);
+        resolve();
+      });
     });
-    jest.spyOn(WebSocket.prototype, 'send').mockImplementation(() => {
-      // console.log(`Mocked send`);
+
+    server.on('connection', (ws) => {
+      // console.error(`New client connected: ${ws.url}`);
+
+      ws.on('close', () => {
+        // console.error('Client disconnected');
+      });
+      ws.on('error', (error) => {
+        // console.error(`Error: ${error}`);
+      });
+      ws.on('message', (message) => {
+        // console.error(`Received message: ${message}`);
+      });
+      ws.on('ping', () => {
+        // console.error('Ping received');
+        ws.pong();
+      });
+      ws.on('open', () => {
+        // console.error('Connection open');
+      });
+
+      // ws.send('Hello Client');
     });
-    wsClient = new WsClient('Unknown', '192.168.58.96');
-  });
+
+    server.on('error', (error) => {
+      // console.error(`Server error: ${error}`);
+    });
+
+    server.on('close', () => {
+      // console.error('Server closed');
+    });
+  }, 10000);
 
   beforeEach(() => {
     //
@@ -34,235 +65,185 @@ describe('WS client', () => {
     //
   });
 
-  afterAll(() => {
-    // wsClient.stop();
-    // Restore the mocked AnsiLogger.log method
-    // (AnsiLogger.prototype.log as jest.Mock).mockRestore();
-  });
+  afterAll(async () => {
+    // Stop the WebSocket client
+    wsClient.stop();
+
+    // Wait for the WebSocket server to close using a Promise
+    await new Promise<void>((resolve, reject) => {
+      server.close((err) => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  }, 10000);
 
   test('Create the wsClient', () => {
+    wsClient = new WsClient('Jest', 'localhost');
+    wsClient.setHost('localhost');
+    expect((wsClient as any).wsHost).toBe('localhost');
+    expect((wsClient as any).wsUrl).toBe(`ws://localhost/rpc`);
     expect(wsClient).not.toBeUndefined();
+    expect(wsClient).toBeInstanceOf(WsClient);
+    wsClient.sendRequest('Shelly.GetStatus');
+    wsClient.stop();
   });
 
-  test('Not starting to be connected', () => {
-    expect(wsClient.isConnected).toBeFalsy();
-    expect(wsClient.isConnecting).toBeFalsy();
-  });
+  test('should connect to the server and start ping pong', async () => {
+    // Await connection to the server
+    const connectPromise = new Promise<WebSocket>((resolve) => {
+      server.once('connection', (ws: WebSocket) => {
+        // The server has received a connection
+        // console.log('Server received connection');
 
-  test('Simulate connection timeout', async () => {
-    wsClient?.start();
+        // Listen for messages from the client
+        ws.once('message', async (message) => {
+          // The server has received a message
+          const msg = JSON.parse(message.toString());
+          // console.log('Server received a message:\n', msg);
+          resolve(ws);
+        });
+      });
+    });
+
+    // Create a WebSocket client and connect to the server and await its connection
+    wsClient = new WsClient('Jest', 'localhost');
+    wsClient.log.logLevel = LogLevel.DEBUG;
+    wsClient.start();
     expect(wsClient.isConnecting).toBeTruthy();
-    expect(wsClient.isConnected).toBeFalsy();
-
-    // Fast-forward until all timers have been executed
-    jest.runAllTimers();
-
-    // Check the state after the timeout
-    expect(wsClient.isConnecting).toBeTruthy();
-    expect(wsClient.isConnected).toBeFalsy();
-
-    jest.useRealTimers();
-
-    // console.log('Waiting for connection timeout', wsClient.isConnecting, wsClient.isConnected);
-    await waiter(
-      'WsClient connection timeout',
-      () => {
-        return !wsClient.isConnecting;
-      },
-      false,
-      300000,
-      500,
-      false,
-    );
-    // console.log('Waiting for connection timeout done', wsClient.isConnecting, wsClient.isConnected);
-    expect(wsClient.isConnecting).toBeFalsy();
-
-    // console.log('Waiting for connection timeout', wsClient.isConnecting, wsClient.isConnected);
-    await waiter(
-      'WsClient connection timeout',
-      () => {
-        return !wsClient.isConnected;
-      },
-      false,
-      300000,
-      500,
-      false,
-    );
-    // console.log('Waiting for connection timeout done', wsClient.isConnecting, wsClient.isConnected);
-    expect(wsClient.isConnected).toBeFalsy();
-  }, 300000);
-
-  // eslint-disable-next-line jest/no-commented-out-tests
-  /*
-  test('Start listening', () => {
-    wsClient?.start(true);
-
-    expect((wsClient as any).wsClient).toBeDefined();
-    expect(wsClient.isConnecting).toBeTruthy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-  */
-
-  test('sendRequest', () => {
-    wsClient.sendRequest('Shelly.GetNone');
-    expect((wsClient as any).requestFrame.method).toBe('Shelly.GetStatus');
-    (wsClient as any)._isConnected = true;
-    wsClient.sendRequest('Shelly.GetNone');
-    expect((wsClient as any).requestFrame.method).toBe('Shelly.GetNone');
-    (wsClient as any)._isConnected = false;
-  });
-
-  test('startPingPong', async () => {
-    (wsClient as any).startPingPong(100);
-    expect((wsClient as any).pingInterval).toBeDefined();
-    await waiter(
-      'WsClient connection timeout',
-      () => {
-        return false;
-      },
-      false,
-      1000,
-      500,
-      false,
-    );
-    (wsClient as any).stopPingPong();
-    // console.log('PingPong readyState:', (wsClient as any).wsClient.readyState);
-    (wsClient as any).wsClient._readyState = WebSocket.OPEN;
-    (wsClient as any).startPingPong(100);
-    await waiter(
-      'WsClient connection timeout',
-      () => {
-        return false;
-      },
-      false,
-      1000,
-      500,
-      false,
-    );
-    (wsClient as any)._isConnected = false;
-    (wsClient as any).wsClient._readyState = WebSocket.CLOSED;
-    (wsClient as any).stopPingPong();
-    (wsClient as any).stopPingPong();
-    (wsClient as any).stopPingPong();
-  }, 60000);
-
-  test('stopPingPong', () => {
-    (wsClient as any).stopPingPong();
-    expect((wsClient as any).pingInterval).toBeUndefined();
-    expect((wsClient as any).pongTimeout).toBeUndefined();
-  });
-
-  test('emit open', () => {
-    (wsClient as any).wsClient?.emit('open');
-    expect(wsClient.isConnecting).toBeFalsy();
+    const ws = await connectPromise;
+    // prettier-ignore
+    await waiter('WsClient isConnected timeout', () => { return wsClient.isConnected; }, true);
+    expect((wsClient as any).auth).toBeFalsy();
     expect(wsClient.isConnected).toBeTruthy();
-    (wsClient as any)._isConnecting = true;
-    (wsClient as any)._isConnected = false;
-    expect((wsClient as any).pingInterval).toBeDefined();
-    (wsClient as any).wsClient?.emit('pong');
-    (wsClient as any).pongTimeout = setTimeout(() => {
+    (wsClient as any).stopPingPong();
+    (wsClient as any).startPingPong(100);
+    // prettier-ignore
+    await waiter('WsClient pong timeout', () => { return (wsClient as any).pongTimeout; }, true);
+    await wait(1000);
+    (wsClient as any).stopPingPong();
+    (wsClient as any).wsClient?.emit('error', new Error('Test error'));
+    (wsClient as any).wsClient?.emit('close');
+    wsClient.stop();
+    expect(wsClient.isConnecting).toBeFalsy();
+    expect(wsClient.isConnected).toBeFalsy();
+    // prettier-ignore
+    await waiter('WsClient close isConnecting timeout', () => { return !wsClient.isConnecting; }, true);
+    // prettier-ignore
+    await waiter('WsClient close isConnected timeout', () => { return !wsClient.isConnected; }, true);
+  }, 10000);
+
+  test('should connect to the server without auth', async () => {
+    // Await connection to the server
+    const connectPromise = new Promise<WebSocket>((resolve) => {
+      server.once('connection', (ws: WebSocket) => {
+        // The server has received a connection
+        // console.log('Server received connection');
+
+        // Listen for messages from the client
+        ws.once('message', async (message) => {
+          // The server has received a message
+          const msg = JSON.parse(message.toString());
+          // console.log('Server received a message:\n', msg);
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, result: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyStatus', params: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyFullStatus', params: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyEvent', params: { events: [{ state: true }] } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, error: true }));
+          ws.send(JSON.stringify({ src: 'Jest' }));
+          ws.send('This is not a JSON message');
+          resolve(ws);
+        });
+      });
+    });
+
+    // Create a WebSocket client and connect to the server and await its connection
+    wsClient = new WsClient('Jest', 'localhost');
+    wsClient.on('response', (response) => {
       //
     });
-    (wsClient as any).stopPingPong();
-    expect((wsClient as any).pingInterval).toBeUndefined();
-    expect((wsClient as any).pongTimeout).toBeUndefined();
-  });
-
-  test('emit error', () => {
-    (wsClient as any).wsClient?.emit('error', new Error('Test error'));
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message auth with no password', () => {
-    const message = { error: { code: 401 }, dst: 'Matterbridge', result: { state: true }, id: (wsClient as any).requestId };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).password = undefined;
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message auth with password', () => {
-    const message = {
-      error: { code: 401, message: '{"message":"emit message auth with password"}' },
-      dst: 'Matterbridge',
-      result: { state: true },
-      id: (wsClient as any).requestId,
-    };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).password = 'password';
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message result', () => {
-    const message = { dst: 'Matterbridge', result: { state: true }, id: (wsClient as any).requestId };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message NotifyStatus', () => {
-    const message = { src: 'device', dst: 'Matterbridge', method: 'NotifyStatus', result: { state: true }, error: undefined };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message NotifyFullStatus', () => {
-    const message = { src: 'device', dst: 'Matterbridge', method: 'NotifyFullStatus', result: { state: true }, error: undefined };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message NotifyEvent', () => {
-    const message = { src: 'device', dst: 'Matterbridge', method: 'NotifyEvent', params: { events: { state: true } }, error: undefined };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message unknown', () => {
-    const message = { nodata: 'nodata' };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit message error', () => {
-    const message = { error: { code: 3232 }, id: (wsClient as any).requestId, dst: 'Matterbridge' };
-    const buffer = Buffer.from(JSON.stringify(message));
-    (wsClient as any).wsClient?.emit('message', buffer);
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('emit close', () => {
-    (wsClient as any).wsClient?.emit('close', new Error('Test error'));
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
-  });
-
-  test('Stop listening', async () => {
+    wsClient.on('update', (params) => {
+      //
+    });
+    wsClient.on('event', (events) => {
+      //
+    });
+    wsClient.log.logLevel = LogLevel.DEBUG;
+    wsClient.start();
+    expect(wsClient.isConnecting).toBeTruthy();
+    const ws = await connectPromise;
+    // prettier-ignore
+    await waiter('WsClient connection timeout', () => { return wsClient.isConnected; }, true);
+    expect((wsClient as any).auth).toBeFalsy();
+    expect(wsClient.isConnected).toBeTruthy();
+    wsClient.sendRequest('Shelly.GetStatus');
     wsClient.stop();
-    expect((wsClient as any).wsClient).toBeDefined();
-    expect((wsClient as any).pingInterval).toBeUndefined();
-    expect((wsClient as any).pongTimeout).toBeUndefined();
-    expect(wsClient.isConnecting).toBeFalsy();
-    expect(wsClient.isConnected).toBeFalsy();
+    // prettier-ignore
+    await waiter('WsClient close timeout', () => { return !wsClient.isConnected; }, true);
+  }, 60000);
 
-    (wsClient as any)._isConnecting = true;
-    jest.useFakeTimers();
+  test('should connect to the server with auth', async () => {
+    // Await connection to the server
+    const connectPromise = new Promise<WebSocket>((resolve) => {
+      server.once('connection', (ws: WebSocket) => {
+        // The server has received a connection
+        // console.log('Server received connection');
+
+        // Listen for messages from the client
+        ws.once('message', (message) => {
+          // The server has received a message
+          const msg = JSON.parse(message.toString());
+          // console.log('Server received message:\n', msg);
+
+          ws.send(
+            JSON.stringify({
+              src: 'Jest',
+              dst: 'Matterbridge' + msg.id,
+              id: msg.id,
+              error: {
+                code: 401,
+                message: JSON.stringify({
+                  auth_type: 'digest',
+                  nonce: 123456,
+                  nc: 123,
+                  realm: 'device_id shelly1minig3-543204547478',
+                  algorithm: 'SHA-256',
+                }),
+              },
+            }),
+          );
+        });
+        ws.once('message', (message) => {
+          // The server has received a message
+          const msg = JSON.parse(message.toString());
+          // console.log('Server received auth message:\n', msg);
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, result: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyStatus', params: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyFullStatus', params: { state: true } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, method: 'NotifyEvent', params: { events: [{ state: true }] } }));
+          ws.send(JSON.stringify({ src: 'Jest', dst: 'Matterbridge' + msg.id, id: msg.id, error: true }));
+          ws.send(JSON.stringify({ src: 'Jest' }));
+          ws.send('This is not a JSON message');
+          resolve(ws);
+        });
+      });
+    });
+
+    // Create a WebSocket client and connect to the server and await its connection
+    wsClient = new WsClient('Jest', 'localhost', 'password');
+    wsClient.log.logLevel = LogLevel.DEBUG;
+    wsClient.start();
+    expect(wsClient.isConnecting).toBeTruthy();
+    const ws = await connectPromise;
+    // prettier-ignore
+    await waiter('WsClient connection timeout', () => { return wsClient.isConnected; }, true);
+    expect((wsClient as any).auth).toBeTruthy();
+    expect(wsClient.isConnected).toBeTruthy();
+    wsClient.sendRequest('Shelly.GetStatus');
     wsClient.stop();
-    jest.runAllTimers();
-    jest.useRealTimers();
+    // prettier-ignore
+    await waiter('WsClient close timeout', () => { return !wsClient.isConnected; }, true);
   }, 60000);
 });
