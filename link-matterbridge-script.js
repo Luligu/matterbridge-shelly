a757a26a6d385cbc5e6e58a296451973ae7cc7ff
Luligu <132135057+Luligu@users.noreply.github.com>
2024-09-30 08:57:39 +0200

    Dev 0.11.0-dev.2

--- link-matterbridge-script.js
+++ link-matterbridge-script.js
@@ -1,1 +1,1 @@
-// Nothing to do in production, just a script to link the matterbridge package to the global npm packages in development
+// Nothing to do in production, just a script to link the matterbridge package in development
