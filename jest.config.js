17c67d4d9fa2e28c5d0ecee94e4268b7b7351a25
Luligu <target.epithet-0h@icloud.com>
2025-11-11 17:25:57 +0100

    Automator: update package v. 2.0.12

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,8 @@ const jestConfig = {
   ...presetConfig,
   testEnvironment: 'node',
   moduleNameMapper: { '^(\\.{1,2}/.*)\\.js$': '$1' }, // Handle ESM imports by removing the .js extension
-  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/vitest/', 'src/net', 'jestHelpers.ts'],
-  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/vitest/', 'src/net', 'jestHelpers.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/', '/vitest/', 'jestHelpers.test.ts'],
+  coveragePathIgnorePatterns: ['/node_modules/', '/dist/', '/vitest/', 'jestHelpers.ts'],
   maxWorkers: '100%',
 };
 
